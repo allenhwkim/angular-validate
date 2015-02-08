8227d11ad4b2d95fa7d13138747ccd3ef2b2c086
Allen Kim <allenhwkim@gmail.com>
2015-02-08 13:32:55 -0500

    made testapp work at github

---
+++ build/scripts/nui-form-for.js
@@ -0,0 +1,154 @@
+var nuiFormFor=angular.module('nui-form-for',[]);
+
+/* global nuiFormFor */
+(function(){
+  'use strict';
+  var mapping = {
+    'required'  : 'ng-required',
+    'minlength' : 'ng-minlength',
+    'maxlength' : 'ng-maxlength',
+    'min'       : 'min',
+    'max'       : 'max',
+    'pattern'   : 'pattern',
+    'type'      : 'type'
+  };
+
+  var controller = ['$scope', '$element', '$attrs', '$http','$q', '$compile',
+    function($scope, $element, $attrs, $http, $q, $compile) {
+      
+      this.compile = function (element) {
+        $compile(element)($scope);
+      };
+      
+      this.getValidationRules = function() {
+        var deferred = $q.defer();
+        if (typeof $scope.validationRule == "object") {
+          deferred.resolve($scope.validationRule);
+        } else {
+          var url = $attrs.validationRule;
+          void 0;
+          $http.get(url).then(function(resp) {
+            deferred.resolve(resp.data);
+          });
+        }
+        return deferred.promise;
+      };
+      
+      this.injectValidationRules = function(formRules) {
+        var form = $element[0];
+        var modelName = $attrs.nuiFormFor;
+        for (var elName in formRules) {
+          var str = 'input[name=VAL], '+
+            'select[name=VAL], '+
+            'textarea[name=VAL]';
+          var selectorStr = str.replace(/VAL/,'"'+elName+'"');
+          var formEls = form.querySelectorAll(selectorStr);
+          var elRules = formRules[elName];
+          
+          for (var i=0; i<formEls.length; i++) {
+            var el = formEls[i];
+            if (el.getAttribute('ng-model')) {
+              void 0;
+            } else {
+              el.setAttribute('ng-model', modelName +'.'+elName);
+              elRules.required = elRules.required || "true";
+              for (var rule in elRules) {
+                var attrKey = mapping[rule], attrVal = elRules[rule];
+                if (el.getAttribute(attrKey)) {
+                  void 0;
+                } else {
+                  if (attrVal) {
+                    (typeof attrVal == "object") && (attrVal = attrVal.value);
+                    el.setAttribute(attrKey, attrVal);
+                  } 
+                }
+              }
+              this.compile(el);
+            }
+          } // for (formEls)
+          
+        } // for (elName in rules)
+      }; // function    
+      
+      this.injectErrorMessages = function(formRules) {
+        var formName = $element[0].getAttribute("name");
+        for (var elName in formRules) {
+          var msgObj = {};
+          var generalMsg = formRules[elName].message;
+          generalMsg && (msgObj.general = generalMsg);
+          for (var key in formRules[elName]) {
+            var rule = formRules[elName][key];
+            if (typeof rule == "object" && rule.message) {
+              msgObj[key] = rule.message;
+            }
+          }
+          if (Object.keys(msgObj).length > 0) {
+            $scope[formName][elName].message = msgObj;
+          }
+        } // for elName in formRules
+      };
+    }
+  ];
+
+  nuiFormFor.directive("nuiFormFor", function() {
+    return {
+      controller: controller, 
+      link: function(scope, element, attrs, ctrl) {
+        var form = element[0];
+        var formName = form.getAttribute('name');
+        if (!formName || form.tagName !== "FORM") {
+          return false; 
+        }
+        form.setAttribute('novalidate','');
+        
+        ctrl.getValidationRules().then(function(formRules) {
+          ctrl.injectValidationRules(formRules);
+          return formRules;
+        }).then(function(formRules) {
+          ctrl.injectErrorMessages(formRules);
+        });
+      }
+    };
+  });
+
+})();
+
+/* global nuiFormFor */
+(function(){
+  'use strict';
+  var mapping = {
+    'required'  : 'ng-required',
+    'minlength' : 'ng-minlength',
+    'maxlength' : 'ng-maxlength',
+    'min'       : 'min',
+    'max'       : 'max',
+    'pattern'   : 'pattern',
+    'type'      : 'type'
+  };
+
+  nuiFormFor.directive("nuiRule", function($compile) {
+    return {
+      require: 'ngModel',
+      link: function(scope, element, attrs) {
+        var el = element[0];
+        var ruleName = attrs.ngRule || attrs.name;
+        var elRules = scope.validationRules[attrs.nuiRule];
+        elRules.required = elRules.required || "true";
+        for (var rule in elRules) {
+          var attrKey = mapping[rule], attrVal = elRules[rule];
+          if (el.getAttribute(attrKey)) {
+            void 0;
+          } else {
+            if (attrVal) {
+              (typeof attrVal == "object") && (attrVal = attrVal.value);
+              el.setAttribute(attrKey, attrVal);
+            } 
+          }
+        }
+        el.removeAttribute("nui-rule"); // to prevent loop
+        $compile(el)(scope);
+      } // link
+    }; // return
+  });
+
+})();
